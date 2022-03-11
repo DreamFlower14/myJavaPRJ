53401d860ada135c13df15ceb826401fa93a7873
data23 <sunghh1004@naver.com>
2022-03-11 17:27:10 +0900

    last

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,64 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("BYE!! My last Java Coding!! ");
-System.out.print("it was good");
+        System.out.print("it was good\n");
+        int num = 2;
+        System.out.printf("i want %d apples\n", num);
+
+        /*
+        int b = 2;
+        int c = a + b;
+        int d = c - a;
+        int e = b * c;
+        int f = e / b;
+        int g = c % b;
+        a++; // a = a + 1;
+        a--; // a = a - 1;
+        ++a;
+        --a;
+
+        System.out.println(a);
+        a = a++;
+        System.out.println(a);
+        a = ++a;
+        System.out.println(a);
+
+        int i = 1;
+        float j = 3.1f;
+        double k = 3.14;
+        System.out.println(i + j);
+        System.out.println((int)(i + j));
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("숫자를 입력해주세요.");
+        sc.nextInt();
+        sc.
+        System.out.println();
+        */
+        int b;
+        int c = 1;
+        for(b = 9 ;b < 82 ; b += 9) {
+            System.out.printf("9 * %d = %d\n", c, b);
+
+            c++;
+        }
+        int d = 13;
+        int e;
+        for(e = 1; e < 10; e++) {
+            //System.out.printf("%d * %d = %d\n", d, e, d * e);
+            System.out.println(d + " * " + e + " = " + d * e);
+        }
+
+        for(int a = 0; a < 10; a++) {
+            System.out.println(a);
+        }
+        int s = 100;
+
+        while(s > 0) {
+            s--;
+            System.out.println(s);
+        }
     }
 }
 // 자바 파일명은 무조건 영문 대문자로 시작해야 함, 대소문자 구별함
