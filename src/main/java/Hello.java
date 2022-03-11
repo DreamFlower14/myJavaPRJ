1a9334e83766788207ff24bea7724083ddeccdb6
data23 <sunghh1004@naver.com>
2022-03-11 16:02:59 +0900

    SECOND

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
-        System.out.println("BYE!! My First Java Coding!! ");
+        System.out.println("BYE!! My last Java Coding!! ");
 
     }
 }
