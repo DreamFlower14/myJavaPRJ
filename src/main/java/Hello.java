ad694fcd6aba7641dead20427f6a78f2f665889d
data23 <sunghh1004@naver.com>
2022-03-11 15:23:12 +0900

    First

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("hello!! My First Java Coding!! ");
+
+    }
+}
+// 자바 파일명은 무조건 영문 대문자로 시작해야 함, 대소문자 구별함
+// public class 파일명 외우기 *****
+// 무조건 main 이라는 함수가 있어야지 자바가 실행함(시작점)
