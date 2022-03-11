46cfabe620fdfb348601e6b48329f22428d57c9c
data23 <sunghh1004@naver.com>
2022-03-11 16:08:08 +0900

    last

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("BYE!! My last Java Coding!! ");
-
+System.out.print("it was good");
     }
 }
 // 자바 파일명은 무조건 영문 대문자로 시작해야 함, 대소문자 구별함
