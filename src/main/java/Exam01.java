d79218e90b3bf149e77082d3f7d8b035c9180751
data23 <sunghh1004@naver.com>
2022-03-16 09:26:38 +0900

    3/16 0923 단축키 연습

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -4,7 +4,10 @@ public class Exam01 {
         System.out.println("인천광역시 서구 마전동 99번길 8, 702동 307호(대주파크빌 아파트)");
         System.out.println("2000.10.04");
         System.out.println("자바 1~9강까지,11,12강까지 준비");
-        System.out.println();
+        System.out.println("왼쪽 ctrl + spacebar 하면 단축키 가능하다.");
+        System.out.println("단축키의 종류에는 psvm과 sout이 있다.");
+        System.out.println("psvm은 public static void main의 단축키");
+        System.out.println("sout은 System.out.println의 단축키이다.");
     }
 
 }
