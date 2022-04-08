d3df3aa71ea02ca8e425bd09721613c1a718703b
data23 <sunghh1004@naver.com>
2022-04-08 15:13:13 +0900

    2022.04.08

--- src/main/java/Switch.java
+++ src/main/java/Switch.java
@@ -8,7 +8,7 @@ public class Switch {
         System.out.printf("1~4 중에 선택하세요 : ");
         a = s.nextInt();
         // 조건이 5개 이상이면 무조건 switch case 쓴다
-        /* switch (a) {
+         switch (a) {
             case 1 :
                 System.out.printf("1을 선택했다\n");
                 break;
@@ -23,9 +23,9 @@ public class Switch {
                 break;
             default:
                 System.out.printf("1부터 4까지 고르랬지.\n");
-        } */
+        }
         // ctrl + alt + L 누르면 알아서 들여쓰기 해준다.
-        //
+        /*
         if (a == 1) {
             System.out.printf("1을 선택했다\n");
         } else if (a == 2) {
@@ -36,6 +36,6 @@ public class Switch {
             System.out.printf("4을 선택했다\n");
         } else {
             System.out.printf("1~4중에 고르랬지");
-        }
+        }*/
     }
 }
