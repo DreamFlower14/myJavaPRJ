89646fe2434ae387bb0b5971fbba5ccd2610cc69
data23 <sunghh1004@naver.com>
2022-04-26 17:22:54 +0900

    2022.04.26

---
+++ src/main/java/Test0522.java
@@ -0,0 +1,13 @@
+public class Test0522 {
+    public static void main(String[] args) {
+        /* While 문 기반 break */
+        int i = 0;
+        while(i < 5){
+            if(i == 3){
+                break;
+            }
+            System.out.println( i);
+            i++;
+        }
+    }
+}
