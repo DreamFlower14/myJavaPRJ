a5e38d551ae43cc3bf51ad5f0295a4299efed895
data23 <sunghh1004@naver.com>
2022-04-29 11:46:19 +0900

    2022.04.29

---
+++ src/main/java/Class0427_5.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Class0427_5 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("이협건");
+        list.add("홍길동");
+        list.add("임꺽정");
+        list.add("고길동");
+
+        int listsize = list.size();
+
+        for(int i = 0; i < listsize; i++){
+            System.out.println("이름 : " + list.get(i));
+        }
+    }
+}
