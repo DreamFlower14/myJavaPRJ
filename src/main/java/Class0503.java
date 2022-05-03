1b70e3bb7b15cb97373fc7654c9f62a2253ba0ab
data23 <sunghh1004@naver.com>
2022-05-03 18:53:08 +0900

    2022.05.03

---
+++ src/main/java/Class0503.java
@@ -0,0 +1,10 @@
+public class Class0503 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{10,20,30,40,50};
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
