be91d881716c9c24b92bef9f8df93855932b00c4
data23 <sunghh1004@naver.com>
2022-05-06 16:15:18 +0900

    2022.05.06

--- src/main/java/Class0503.java
+++ src/main/java/Class0503.java
@@ -3,6 +3,8 @@ public class Class0503 {
         int[] arr = new int[]{10,20,30,40,50};
         int sum = 0;
         for(int i = 0; i < arr.length; i++){
+            System.out.println(i);
+            System.out.println(arr[i]);
             sum += arr[i];
         }
         System.out.println(sum);
---
+++ src/main/java/Class0506.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Class0506 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a [] = new int[10];
+
+        for(int i = 0; i < 10; i++){
+            a[i] = s.nextInt();
+        }
+        int b = s.nextInt();
+        System.out.printf("%d",a[b-1]);
+    }
+}
