c21819fbd6172155de3163104f925b244764b31e
data23 <sunghh1004@naver.com>
2022-05-09 16:48:14 +0900

    2022.05.09

---
+++ src/main/java/Class0509.java
@@ -0,0 +1,17 @@
+public class Class0509 {
+    public static void main(String[] args) {
+        int aa[] = {100,200,300,400};
+        int bb[] = new int[] {100,200,300};
+
+        int cc[];
+        cc = new int[] {100,200};
+        int[] dd = new int[1];
+        dd[0] = 100;
+        int i;
+
+        for (i = 0; i < 4; i++) {
+            System.out.printf("aa[%d] ==> %d\t", i, aa[i]);
+            System.out.println();
+        }
+    }
+}
