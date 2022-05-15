e1f370ecb48fbcc650bdc861f4c05df6d379fab9
data23 <sunghh1004@naver.com>
2022-05-15 14:11:35 +0900

    2022.05.15

---
+++ src/main/java/Class/C0512.java
@@ -0,0 +1,16 @@
+package Class;
+import java.util.Scanner;
+public class C0512 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int[] li = new int[a];
+        int sum = 0;
+        for (int i = 0; i < a; i++) {
+            li[i] = s.nextInt();
+            sum = li[i];
+        }
+
+    }
+}
