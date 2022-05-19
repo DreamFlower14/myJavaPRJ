2195e796421570066b7e68f482f6d0693642e0a2
data23 <sunghh1004@naver.com>
2022-05-19 10:48:06 +0900

    2022.05.18

---
+++ src/main/java/Class/C0518_6.java
@@ -0,0 +1,13 @@
+package Class;
+
+public class C0518_6 {
+    public static void main(String[] args) {
+        String str = "  한글  ABCD    efgh    ";
+
+        System.out.println("원 문자열 : [" + str + "]" );
+        System.out.println("대문자로 : [" + str.toUpperCase() + "]");
+        System.out.println("소문자로 : [" + str.toLowerCase() + "]");
+        System.out.println("공백 제거 : [" + str.trim() + "]");
+        System.out.println("전체공백 제거 : [" + str.replace(" ", "") + "]");
+    }
+}
