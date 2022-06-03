edd6f7bc6da4427abaa6d54d3e5870235b2caab8
data23 <sunghh1004@naver.com>
2022-06-03 16:30:26 +0900

    2022.06.03

---
+++ src/main/java/Class/C0524_3.java
@@ -0,0 +1,21 @@
+package Class;
+
+import java.util.Scanner;
+
+public class C0524_3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+
+        if (!str.startsWith("(")){
+            System.out.print("(");
+        }
+        for (int i = 0; i < str.indexOf("안"); i++) {
+            System.out.print(str.charAt(i));
+        }
+        if (!str.endsWith(")")){
+            System.out.print(")");
+        }
+
+    }
+}
