e0e48c45b6a8f874b24ab0eb0729f0d0b6ced6b3
data23 <sunghh1004@naver.com>
2022-06-15 14:19:01 +0900

    2022.06.15

---
+++ src/main/java/Class/C0615_2.java
@@ -0,0 +1,51 @@
+package Class;
+
+class Car1 {
+    String color;
+    int speed;
+    
+    void upSpeed(int value) {
+        speed += value;
+        System.out.println("속도를 "+ speed +"올립니다");
+    }
+
+    void downSpeed(int value) {
+        speed -= value;
+        System.out.println("속도를 "+ speed +"내립니다");
+    }
+}
+
+class Sedan extends Car1{
+    int seatNum;
+    
+    int getSeatNum() {
+        return seatNum;
+    }
+}
+
+class Truck extends Car1{
+    int catacity;
+
+    int getCatacity() {
+        return catacity;
+    }
+}
+
+public class C0615_2 {
+    public static void main(String[] args) {
+        
+        Sedan sedan1 = new Sedan();
+        Truck truck1 = new Truck();
+        
+        sedan1.upSpeed(300);
+        truck1.upSpeed(100);
+        
+        sedan1.seatNum = 5;
+        truck1.catacity = 50;
+
+        System.out.println("승용차 속도는 " + sedan1.speed + "km, 좌석수는 " + sedan1.getSeatNum() + "개 입니다");
+        System.out.println("트럭 속도는 " + truck1.speed + "km, 적재량은 " + truck1.getCatacity() + "톤 입니다");
+        
+    }
+}
+
